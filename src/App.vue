dbb167b66bbad88d824728af5b1ac680f35c79ef
ginevrabotrugno <ginevrabotrugno.gb@gmail.com>
2024-07-16 17:40:14 +0200

    feate: create AppHeader static structure

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <script>
+import AppHeader from './components/AppHeader.vue';
+
 export default {
   name: 'App',
+  components: {
+    AppHeader
+  }
 }
 </script>
 
 <template>
-
+  <AppHeader/>
 </template>
 
 <style lang="scss">
