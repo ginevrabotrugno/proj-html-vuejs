3709a4ee8461ce20beb13ed0d30bb6922940b350
simone menichetti <simon3menichettui95@gmail.com>
2024-07-18 17:41:50 +0200

    fix:(animation logocarousel)

--- src/components/LogoCarousel.vue
+++ src/components/LogoCarousel.vue
@@ -1,3 +1,4 @@
+
 <script>
 export default {
   name: 'LogoCarousel',
@@ -18,24 +19,27 @@ export default {
   computed: {
     visibleImages() {
       let start = this.currentIndex;
-      let end = this.currentIndex + 5; 
+      let end = this.currentIndex + 5;
       if (end > this.images.length) {
         return this.images.slice(start).concat(this.images.slice(0, end - this.images.length));
       } else {
         return this.images.slice(start, end);
       }
     },
+    carouselWidth() {
+      return this.images.length * 100 + '%';
+    }
   },
   methods: {
     startAutoScroll() {
-      this.autoScrollInterval = setInterval(this.nextAuto, 6000); 
+      this.autoScrollInterval = setInterval(this.nextAuto, 3000);
     },
     stopAutoScroll() {
       clearInterval(this.autoScrollInterval);
     },
     nextAuto() {
       if (!this.userInteracted) {
-        this.currentIndex = (this.currentIndex + 1) % this.images.length;
+        this.next();
       } else {
         this.userInteracted = false;
       }
@@ -60,15 +64,20 @@ export default {
 
 
 <template>
- <div class="carousel-container">
+  <div class="carousel-container">
     <button @click="prev" class="carousel-button prev"><i class="fa-solid fa-chevron-left"></i></button>
     <div class="carousel">
       <div
-        class="carousel-item"
-        v-for="(image, index) in visibleImages"
-        :key="index"
+        class="carousel-inner"
+        :style="{ width: carouselWidth, transform: `translateX(-${currentIndex * 20}%)` }"
       >
-        <img :src="image" :alt="'Image ' + index" />
+        <div
+          class="carousel-item"
+          v-for="(image, index) in images.concat(images.slice(0, 5))" 
+          :key="index"
+        >
+          <img :src="image" :alt="'Image ' + index" />
+        </div>
       </div>
     </div>
     <button @click="next" class="carousel-button next"><i class="fa-solid fa-chevron-right"></i></button>
@@ -77,6 +86,7 @@ export default {
 
 <style scoped lang="scss">
 @use '/src/style/general.scss' as *;
+
 .carousel-container {
   position: relative;
   display: flex;
@@ -87,13 +97,18 @@ export default {
 }
 
 .carousel {
-  display: flex;
   overflow: hidden;
-  width: 80%; 
+  width: 80%;
+  display: flex;
+}
+
+.carousel-inner {
+  display: flex;
+  transition: transform 0.5s ease-in-out;
 }
 
 .carousel-item {
-  flex: 1 0 18%; 
+  flex: 0 0 20%;
   margin: 0 5px;
 }
 
@@ -107,9 +122,9 @@ export default {
   background-color: rgba(0, 0, 0, 0.5);
   border: none;
   color: white;
-  padding: 20px;
+  padding: 10px;
   cursor: pointer;
-  font-size: 24px;
+  font-size: 16px;
   position: absolute;
   top: 50%;
   transform: translateY(-50%);
