ea0d88e5afbafbe4440e6b9dbd8df40bb67493a7
simone menichetti <simon3menichettui95@gmail.com>
2024-07-18 11:48:46 +0200

    feat:(add general styles scss counter components)

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -27,7 +27,7 @@ export default {
       const speeds = {
         counter1: 5000, // Years of Experience
         counter2: 2000, // Everyday Online Delivery (più veloce)
-        counter3: 4000, // Client Satisfaction
+        counter3: 5000, // Client Satisfaction
         counter4: 5000  // Award Winning
       };
 
@@ -35,7 +35,13 @@ export default {
         const updateCount = () => {
           const target = +counter.getAttribute('data-target');
           const count = +counter.innerText.replace(/[+K]/g, '');
-          const increment = (target / speeds[counter.id]) * 100;
+          let increment;
+
+          if (counter.id === 'counter3') {
+            increment = (target * 1000) / speeds[counter.id]; 
+          } else {
+            increment = (target / speeds[counter.id]) * 100; 
+          }
 
           if (count < target) {
             counter.innerText = Math.ceil(count + increment) + (counter.id === 'counter3' ? 'K' : '+');
@@ -53,11 +59,26 @@ export default {
 </script>
 
 <template>
-  <div class="banner">
-    <div class="counter" id="counter1" data-target="29">0+</div>
-    <div class="counter" id="counter2" data-target="200">0+</div>
-    <div class="counter" id="counter3" data-target="20">0K</div>
-    <div class="counter" id="counter4" data-target="30">0+</div>
+ <div class="banner">
+    <div class="counter-container">
+      <div class="counter" id="counter1" data-target="29">0+</div>
+      <div class="label">Years of Experienced</div>
+    </div>
+    <img class="separator" src="/src/assets/counter-shap1.png" alt="separator">
+    <div class="counter-container">
+      <div class="counter" id="counter2" data-target="200">0+</div>
+      <div class="label">Everyday Online Delivery</div>
+    </div>
+    <img class="separator" src="/src/assets/counter-shap1.png" alt="separator">
+    <div class="counter-container">
+      <div class="counter" id="counter3" data-target="20">0k</div>
+      <div class="label">Client Satisfaction</div>
+    </div>
+    <img class="separator" src="/src/assets/counter-shap1.png" alt="separator">
+    <div class="counter-container">
+      <div class="counter" id="counter4" data-target="30">0+</div>
+      <div class="label">Award Winning</div>
+    </div>
   </div>
 </template>
 
@@ -70,13 +91,35 @@ export default {
   display: flex;
   justify-content: space-around;
   align-items: center;
-  margin-bottom: 100px;
-  padding: 20px;
-  color: #fff;
+  width: 100%;
+  height: 300px;
+  margin-left: 10px;
+  color: #ef9e03;
+}
+
+.counter-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
 }
 
 .counter {
-  font-size: 2rem;
+  font-size: 6rem;
+  font-weight: bolder;
+  text-align: center;
+}
+
+.label {
+  margin-top: 10px;
+  font-size: 2.2rem;
+  color: #fff;
   font-weight: bold;
+}
+
+.separator {
+  height: 200px; 
+  width: 20px;
+  margin: 0 10px;
 }
 </style>
