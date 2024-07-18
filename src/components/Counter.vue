eca8b35335c3ccbf4dc26db22a4493662797d650
simone menichetti <simon3menichettui95@gmail.com>
2024-07-18 10:57:09 +0200

    fix:(count components counter )

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,25 +1,46 @@
-<script>
 
+<script>
 export default {
-    name: 'Counter',
-    mounted() {
-    this.startCounters();
+  name: 'Counter',
+  mounted() {
+    this.setupIntersectionObserver();
   },
   methods: {
+    setupIntersectionObserver() {
+      const options = {
+        root: null,
+        threshold: 0.1
+      };
+      const observer = new IntersectionObserver(this.handleIntersection, options);
+      observer.observe(this.$el);
+    },
+    handleIntersection(entries) {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          this.startCounters();
+        }
+      });
+    },
     startCounters() {
-        const counters = this.$el.querySelectorAll('.counter');
-        const speed = 5000;
+      const counters = this.$el.querySelectorAll('.counter');
 
-        counters.forEach(counter => {
+      const speeds = {
+        counter1: 5000, // Years of Experience
+        counter2: 2000, // Everyday Online Delivery (più veloce)
+        counter3: 4000, // Client Satisfaction
+        counter4: 5000  // Award Winning
+      };
+
+      counters.forEach(counter => {
         const updateCount = () => {
           const target = +counter.getAttribute('data-target');
           const count = +counter.innerText.replace(/[+K]/g, '');
-          const increment = target / speed;
+          const increment = (target / speeds[counter.id]) * 100;
 
           if (count < target) {
             counter.innerText = Math.ceil(count + increment) + (counter.id === 'counter3' ? 'K' : '+');
             setTimeout(updateCount, 100);
-        } else {
+          } else {
             counter.innerText = target + (counter.id === 'counter3' ? 'K' : '+');
           }
         };
@@ -28,30 +49,34 @@ export default {
       });
     }
   }
-}   
+};
 </script>
 
 <template>
-    <div class="banner">
-        <div class="counter" id="counter1" data-target="29">0+</div>
-        <div class="counter" id="counter2" data-target="200">0+</div>
-        <div class="counter" id="counter3" data-target="20">0K</div>
-        <div class="counter" id="counter4" data-target="30">0+</div>
-    </div>
-    
-   
+  <div class="banner">
+    <div class="counter" id="counter1" data-target="29">0+</div>
+    <div class="counter" id="counter2" data-target="200">0+</div>
+    <div class="counter" id="counter3" data-target="20">0K</div>
+    <div class="counter" id="counter4" data-target="30">0+</div>
+  </div>
 </template>
+
 
 <style scoped lang="scss">
 @use '../style/partials/variables' as *;
 
-.banner{
-    background-image: url('src/assets/milesim-bg.jpg');
-    display: flex;
-    justify-content: center;
-    margin-bottom: 100px;
+.banner {
+  background-image: url('src/assets/milesim-bg.jpg');
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  margin-bottom: 100px;
+  padding: 20px;
+  color: #fff;
 }
 
-
-
+.counter {
+  font-size: 2rem;
+  font-weight: bold;
+}
 </style>
