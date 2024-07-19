73b75be3f1d5e8d5094fa41da39f689d48a2f3ff
ValerioViteritti <vviteritti@gmail.com>
2024-07-19 17:56:40 +0200

    fix: AppFooter and BestSale25 indentation

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -6,6 +6,7 @@ export default {
 
 <template>
     <div class="container">
+        <!-- Sezione superiore della newsletter -->
         <div class="rowUp">
             <div id="up">
                 <div>SUBSCRIBE OUR NEWSLETTER</div>
@@ -17,8 +18,11 @@ export default {
                 </div>
             </div>
         </div>
+
+        <!-- Sezione centrale con contenuti vari -->
         <div class="rowCenter">
             <div id="center">
+                <!-- Informazioni Gogrin -->
                 <div id="gogrin">
                     <img src="../assets/footer-logo.png" alt="footer-logo">
                     <div id="gogrinText">War should never be entered upon until every agency of peace has failed not to be good</div>
@@ -30,16 +34,17 @@ export default {
                     </div>
                 </div>
 
+                <!-- Link di emergenza -->
                 <div id="emergencyLink">
-                    <span>
-                        Emergency Link.
-                    </span>
+                    <span>Emergency Link.</span>
                     <a href="#">Organic Food Shop</a>
                     <a href="#">Our Term & Condition.</a>
                     <a href="#">Gogrin Faq Section</a>
                     <a href="#">Our Team Member</a>
                     <a href="#">Our Latest News Feed</a>
                 </div>
+
+                <!-- Sezione blog -->
                 <div id="ourLatestBlog">
                     <span>Our Latest Blog.</span>
                     <div>
@@ -49,7 +54,6 @@ export default {
                             <br>
                             <span>December 17, 2022</span>
                         </div>
-                        
                     </div>
                     <div>
                         <img src="../assets/blo2-70x70.jpg" alt="blo2-70x70">
@@ -58,26 +62,27 @@ export default {
                             <br>
                             <span>December 16, 2022</span>
                         </div>
-                        
                     </div>
                 </div>
+
+                <!-- Galleria -->
                 <div id="ourGallery">
                     <span>Our Gallery</span>
                     <div id="galleryImg">
                         <img src="../assets/blo1-100x100.jpg" alt="">
                         <img src="../assets/blo9-100x100.jpg" alt="">
                         <img src="../assets/blo8-100x100.jpg" alt="">
-
                     </div>
                     <div>
                         <img src="../assets/blo7-100x100.jpg" alt="">
                         <img src="../assets/blo4-100x100.jpg" alt="">
                         <img src="../assets/blo3-100x100.jpg" alt="">
-                        
                     </div>
                 </div>
             </div>
         </div>
+
+        <!-- Sezione inferiore con copyright -->
         <div class="rowDown">
             <div id="down">
                 <div>Copyright © gogrin all rights reserved.</div>
@@ -89,7 +94,6 @@ export default {
             </div>
         </div>
     </div>
-
 </template>
 
 <style scoped lang="scss">
@@ -98,8 +102,6 @@ export default {
 * {
     text-decoration: none;
 }
-
-
 
 .container {
     position: relative;
@@ -114,26 +116,19 @@ export default {
         left: 20%;
 
         div#up {
-
             width: 90%;
             margin: 0 auto;
             height: 18vh;
-            // font
             color: white;
             font-size: 2.2rem;
             font-weight: 600;
-
-            // flex
             display: flex;
             justify-content: space-between;
             align-items: center;
             flex-wrap: wrap;
 
-            
-
             div.email {
                 width: 500px;
-                // flex
                 display: flex;
                 flex-wrap: wrap;
 
@@ -150,16 +145,14 @@ export default {
                     font-size: 1.1rem;
                     background-color: $green;
                     cursor: pointer;
-                    // flex
                     display: flex;
                     justify-content: center;
                     align-items: center;
                     transition: background-color $time;
 
-                    &:hover{
+                    &:hover {
                         background-color: #050e62;
                     }
-
                 }
             }
         }
@@ -170,138 +163,118 @@ export default {
         background-size: cover;
         background-position: center;
 
-        
-
         div#center {
             width: 90%;
             margin: 0 auto;
-            // height: 50vh;
             color: white;
             padding: 180px 0 65px;
-            
-            // flex
             display: flex;
             justify-content: center;
             align-items: start;
             gap: 20px;
 
-            #gogrin{
+            #gogrin {
                 width: calc(100% / 20 * 3);
                 margin-left: 60px;
-                // margin-top: 200px;
-                
-                div#gogrinText{
+
+                div#gogrinText {
                     margin: 25px 0;
                 }
 
-                div#logoSocial{
-
-                    
-                    
-
-                    // flex
+                div#logoSocial {
                     display: flex;
                     justify-content: start;
                     align-items: center;
                     flex-wrap: wrap;
                     gap: 15px;
 
-                    div{
+                    div {
                         background-color: $green;
                         height: 40px;
                         width: 40px;
                         border-radius: 5px;
                         cursor: pointer;
-                        // flex
                         display: flex;
                         justify-content: center;
                         align-items: center;
                         transition: background-color $time;
 
-                        &:hover{
-                        background-color: $orange;
-                        
+                        &:hover {
+                            background-color: $orange;
+                        }
                     }
 
+                    .fa-brands {
+                        padding: 15px;
                     }
                 }
-                .fa-brands{
-                    
-                    padding: 15px;
-                    
-                }
-
             }
 
-            #emergencyLink{
+            #emergencyLink {
                 width: calc(100% / 20 * 2);
-                // margin-top: 200px;
-                span{
-                    font-size: 1.3rem;
-                    font-weight: 600;
-                }
-                // flex
                 display: flex;
                 flex-direction: column;
                 gap: 20px;
 
-                a{
+                span {
+                    font-size: 1.3rem;
+                    font-weight: 600;
+                }
+
+                a {
                     transition: color $time;
-                    &:hover{
+
+                    &:hover {
                         color: $orange;
                     }
                 }
-                
             }
 
-            #ourLatestBlog{
+            #ourLatestBlog {
                 width: calc(100% / 20 * 4);
-                // margin-top: 200px;
-                
-                span:first-child{
+                display: flex;
+                flex-direction: column;
+                gap: 15px;
+
+                span:first-child {
                     font-size: 1.3rem;
                     font-weight: 600;
                     margin-bottom: 10px;
                 }
 
-                // flex
-                display: flex;
-                flex-direction: column;
-                gap: 15px;
-
-                &>div{
+                &>div {
                     display: flex;
                     gap: 10px;
                 }
 
-                img{
+                img {
                     height: 70px;
                     width: 70px;
                 }
 
-                a{
+                a {
                     font-weight: 600;
                     transition: color $time;
 
-                    &:hover{
+                    &:hover {
                         color: $orange;
                     }
                 }
             }
 
-            #ourGallery{
+            #ourGallery {
                 width: calc(100% / 20 * 4);
-                // margin-top: 200px;
-                span{
+
+                span {
                     font-size: 1.3rem;
                     font-weight: 600;
                 }
 
-                div#galleryImg{
-                   margin-top: 20px;
+                div#galleryImg {
+                    margin-top: 20px;
                 }
 
-                img{
+                img {
                     margin: 5px;
                     cursor: pointer;
                     height: 80px;
@@ -313,37 +286,23 @@ export default {
     .rowDown {
         background-color: $green;
 
-
         div#down {
             width: 60%;
             margin: 0 auto;
             height: 8vh;
-            // font
             color: white;
-            // flex
             display: flex;
             justify-content: space-between;
             align-items: center;
 
-            div {
+            div a {
+                font-size: 1.2rem;
+                color: white;
+                margin: 20px;
+                transition: color $time;
 
-                // &:first-child {
-                //     margin-left: 50px;
-                // }
-
-                a {
-                    font-size: 1.2rem;
-                    color: white;
-                    margin: 20px;
-                    transition: color $time;
-
-                    &:hover{
-                        color: $orange;
-                    }
-
-                    // &:last-child {
-                    //     margin-right: 50px;
-                    // }
+                &:hover {
+                    color: $orange;
                 }
             }
         }
--- src/components/BestSale25.vue
+++ src/components/BestSale25.vue
@@ -6,11 +6,14 @@ export default {
     name: 'BestSale25',
     setup() {
         const route = useRoute();
-        
+
+        // Mappa delle immagini di sfondo in base al nome della rotta
         const backgroundImages = {
             Home: 'src/assets/call-to-bg.jpg',
             About: 'src/assets/fe1.jpg',
         };
+
+        // Stile computato per l'immagine di sfondo
         const backgroundImageStyle = computed(() => {
             const routeName = route.name;
             const backgroundImage = backgroundImages[routeName] || backgroundImages.default;
@@ -25,9 +28,8 @@ export default {
         return {
             backgroundImageStyle
         };
-
     }
-}   
+}
 </script>
 
 <template>
@@ -38,18 +40,10 @@ export default {
                 <h2>Best organic food up to 25% off for all product</h2>
                 <p>The most beautiful people we have known are those who have known defeat, known suffering, known struggle, known loss, and have found their way out of those depths. The most beautiful people we have known are those who have known defeat, known suffering</p>
                 <div>
-                <div>
                     <div><i class="fa-brands fa-viber"></i> +088123 56 906</div>
-                </div>
-                <div>
-                    <div>APOINMENT</div>
+                    <div>APPOINTMENT</div>
                 </div>
             </div>
-
-            </div>
-            
-            
-            
         </div>
     </div>
 </template>
@@ -57,81 +51,67 @@ export default {
 <style scoped lang="scss">
 @use '../style/partials/variables' as *;
 
-.container_90{
-    
-    .row{
+.container_90 {
+    .row {
         background-size: cover;
         background-position: center;
         width: 100%;
         height: 400px;
         color: white;
-        // flex
         display: flex;
         justify-content: center;
 
-        
-
-        .col{
+        .col {
             width: 62%;
             text-align: center;
-
-            // flex
             display: flex;
             flex-direction: column;
             align-items: center;
             justify-content: center;
             gap: 30px;
 
-            span{
+            span {
                 font-size: 1.2rem;
                 font-weight: 600;
             }
 
-            h2{
+            h2 {
                 font-size: 2.5rem;
             }
 
-            &>div{
-            display: flex;
-            gap: 30px;
+            &>div {
+                display: flex;
+                gap: 30px;
 
-            &>div{
-
-                &:nth-child(1){
-                    background-color: $orange;
-                    font-weight: 600;
-                    cursor: pointer;
-                    padding: 15px 25px;
-                    border-radius: 5px;
-                    transition: background-color $time;
-
-                    &:hover{
-                        background-color: $green;
-                    }
-                }
-
-                &:nth-child(2){
-                    background-color: $green;
-                    font-weight: 600;
-                    cursor: pointer;
-                    padding: 15px 25px;
-                    border-radius: 5px;
-                    transition: background-color $time;
-
-                    &:hover{
+                &>div {
+                    &:nth-child(1) {
                         background-color: $orange;
+                        font-weight: 600;
+                        cursor: pointer;
+                        padding: 15px 25px;
+                        border-radius: 5px;
+                        transition: background-color $time;
+
+                        &:hover {
+                            background-color: $green;
+                        }
+                    }
+
+                    &:nth-child(2) {
+                        background-color: $green;
+                        font-weight: 600;
+                        cursor: pointer;
+                        padding: 15px 25px;
+                        border-radius: 5px;
+                        transition: background-color $time;
+
+                        &:hover {
+                            background-color: $orange;
+                        }
                     }
                 }
-                
             }
         }
-
-
-        }
-
-        
     }
 }
-
-
 </style>
